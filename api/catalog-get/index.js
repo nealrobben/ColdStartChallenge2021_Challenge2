0ec4c884b2f8dff3fdc181ca5dcbcc9f8e512341
nealrobben <neal.robben@cegeka.com>
2021-03-09 18:44:17 +0100

    Added code to retrieve icecreams

--- api/catalog-get/index.js
+++ api/catalog-get/index.js
@@ -1,7 +1,52 @@
-const data = require('../shared/catalog-data');
+//const data = require('../shared/catalog-data');
 
-module.exports = async function (context, req) {
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request
+var TYPES = require('tedious').TYPES;
+
+const executeSQL = (context) => {
+	
+	// Create Connection object
+    const connection = new Connection({
+        server: 'coldstartchallenge.database.windows.net',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'neal',
+                password: 'AzureAzure123',
+            }
+        },
+        options: {
+            database: 'ColdStartChallenge',
+            encrypt: true
+        }
+    });
+	
+	// Create the command to be executed
+    const request = new Request('SELECT * FROM icecreams FOR JSON PATH;', (err) => {
+        if (err) {
+            context.log.error(err);            
+            context.res.status = 500;
+            context.res.body = "Error executing T-SQL command";
+        } else {
+            context.res = {
+                body: result
+            }   
+        }
+        context.done();
+    });
+	
+	// Connect
+    connection.connect();
+	connection.execSql(request);
+	
+}
+
+module.exports = function (context, req) {
   try {
+	  
+	  executeSQL(context);
+	  
     const items = await data.getCatalog();
     context.res.status(200).send(items);
   } catch (error) {
