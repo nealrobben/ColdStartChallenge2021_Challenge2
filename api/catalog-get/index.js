6664fbcdf97a2bb7db1879ded833714f7170fc31
nealrobben <neal.robben@cegeka.com>
2021-03-11 09:25:56 +0100

    Uncommented SQL

--- api/catalog-get/index.js
+++ api/catalog-get/index.js
@@ -1,46 +1,46 @@
 //const data = require('../shared/catalog-data');
 
-// var Connection = require('tedious').Connection;
-// var Request = require('tedious').Request
-// var TYPES = require('tedious').TYPES;
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request
+var TYPES = require('tedious').TYPES;
 
-// const executeSQL = (context) => {
+const executeSQL = (context) => {
 	
-	// // Create Connection object
-    // const connection = new Connection({
-        // server: 'coldstartchallenge.database.windows.net',
-        // authentication: {
-            // type: 'default',
-            // options: {
-                // userName: 'neal',
-                // password: 'AzureAzure123',
-            // }
-        // },
-        // options: {
-            // database: 'ColdStartChallenge',
-            // encrypt: true
-        // }
-    // });
+	// Create Connection object
+    const connection = new Connection({
+        server: 'coldstartchallenge.database.windows.net',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'neal',
+                password: 'AzureAzure123',
+            }
+        },
+        options: {
+            database: 'ColdStartChallenge',
+            encrypt: true
+        }
+    });
 	
-	// // Create the command to be executed
-    // const request = new Request('SELECT * FROM icecreams FOR JSON PATH;', (err) => {
-        // if (err) {
-            // context.log.error(err);            
-            // context.res.status = 500;
-            // context.res.body = "Error executing T-SQL command";
-        // } else {
-            // context.res = {
-                // body: result
-            // }   
-        // }
-        // context.done();
-    // });
+	// Create the command to be executed
+    const request = new Request('SELECT * FROM icecreams FOR JSON PATH;', (err) => {
+        if (err) {
+            context.log.error(err);            
+            context.res.status = 500;
+            context.res.body = "Error executing T-SQL command";
+        } else {
+            context.res = {
+                body: result
+            }   
+        }
+        context.done();
+    });
 	
-	// // Connect
-    // connection.connect();
-	// connection.execSql(request);
+	// Connect
+    connection.connect();
+	connection.execSql(request);
 	
-// }
+}
 
 module.exports = function (context, req) {
   try {
