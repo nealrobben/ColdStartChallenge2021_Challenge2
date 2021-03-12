545763a5fd939218108a95c33be7d56547b9220e
Neal Robben <neal.robben@gmail.com>
2021-03-12 22:42:52 +0100

    Get catalog

--- api/shared/catalog-data.js
+++ api/shared/catalog-data.js
@@ -1,33 +1,56 @@
-const fs = require('fs').promises;
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
+const { Connection, Request } = require("tedious");
 
 async function getCatalog() {
-  // console.log('using static data.');
-  // var stringData = await fs.readFile('./shared/catalog.json', 'utf8');
-  // const data = JSON.parse(stringData);
-  // return data.icecreams;
-  
-  // Create connection to database
-	var config = {
-	  server: 'coldstartchallenge.database.windows.net',
-	  authentication: {
-		  type: 'default',
-		  options: {
-			  userName: 'neal', // update me
-			  password: 'AzureAzure123' // update me
-		  }
-	  },
-	  options: {
-		database: 'ColdStartChallenge'
-	  }
-	}
-
-	var connection = new Connection(config);
-	var request = new Request('SELECT * FROM icecreams;')
+  return new Promise((resolve, reject) => {
+    
+	const conn = new Connection({
+        server: 'coldstartchallenge.database.windows.net',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'neal',
+                password: 'AzureAzure123',
+            }
+        },
+        options: {
+            database: 'ColdStartChallenge',
+            encrypt: true
+        }
+    });
+    const result = [];
 	
-	connection.execSql(request);
+    const request = new Request(
+      `SELECT Id, Name, Description, ImageUrl FROM Icecreams`,
+      (err, rowCount) => {
+        if (err) { 
+          reject(err);
+        }
+        else {
+          resolve(result);
+        }
+  });
+
+  request.on("row", columns => {
+    
+    const data = {};
+    columns.forEach(column => {
+      data[column.metadata.colName] = column.value;
+    });
+	
+    result.push(data);
+  });
+
+  conn.on("connect", err => {
+    if (err) {
+      reject(err);
+    } else {
+      conn.execSql(request);
+    }
+  });
+  
+  conn.connect();
+  
+});
 }
 
 module.exports = { getCatalog };
