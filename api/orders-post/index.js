002f6a9c5a2ff811280b07a6134dba45d91df744
Neal Robben <neal.robben@gmail.com>
2021-03-13 18:49:18 +0100

    Experiment

--- api/orders-post/index.js
+++ api/orders-post/index.js
@@ -22,7 +22,42 @@ module.exports = async function (context, req) {
 
   try {
     
-	Insert(user.userDetails, new Date(), req.body.Id, "New", null, req.body.ShippingAddress, null, null);
+	const conn = new Connection({
+        server: 'coldstartchallenge.database.windows.net',
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'neal',
+                password: 'AzureAzure123',
+            }
+        },
+        options: {
+            database: 'ColdStartChallenge',
+            encrypt: true
+        }
+    });
+	
+	    // request = new Request(
+        // 'INSERT INTO Orders (User, Date, IcecreamId, DriverId, FullAddress, LastPosition) OUTPUT INSERTED.Id VALUES (@User, @Date, @IcecreamId, @DriverId, @FullAddress, @LastPosition);',
+        // function(err, rowCount, rows) {
+        // if (err) {
+            // callback(err);
+        // } else {
+            // callback(null, '', '');
+        // }
+        // });
+    // request.addParameter('User', TYPES.NVarChar, user);
+    // request.addParameter('Date', TYPES.Date, date);
+	// request.addParameter('IcecreamId', TYPES.Int, icecreamId);
+	// request.addParameter('DriverId', TYPES.Int, driverId);
+	// request.addParameter('FullAddress', TYPES.Int, fullAddress);
+	// request.addParameter('LastPosition', TYPES.Int, lastPosition);
+
+    // Execute SQL statement
+    
+	//connection.execSql(request);
+	
+	//Insert(user.userDetails, new Date(), req.body.Id, "New", null, req.body.ShippingAddress, null, null);
 
     context.res.status(201).json(order);
     context.done();
