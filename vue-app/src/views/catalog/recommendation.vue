919c2a62ea88af18c6d3a206b49c92feabe58400
nealrobben <neal.robben@cegeka.com>
2021-03-15 17:29:24 +0100

    Experiment

--- vue-app/src/views/catalog/recommendation.vue
+++ vue-app/src/views/catalog/recommendation.vue
@@ -17,12 +17,37 @@ export default {
     CardContent,
     ListHeader,
   },
+  async created() {
+	await this.getRecommendedIcecream();
+  },
+    computed: {
+    ...mapGetters('catalog', { recommendation: 'recommendation' }),
+    ...mapGetters('catalog', { catalog: 'catalog' }),
+    icecreamRecommendation() {
+      if (this.catalog && this.recommendation) {
+        const icecreamid = Number.parseInt(this.recommendation.icecreamId, 10);
+        return this.catalog.filter((c) => c.Id === icecreamid)[0];
+      }
+      return null;
+    },
+  },
+  methods: {
+    ...mapActions('catalog', ['getCatalogRecommendation']),
+    async getRecommendedIcecream() {
+      this.errorMessages = undefined;
+      try {
+        this.getCatalogRecommendation();
+      } catch (error) {
+        this.errorMessages = error.message;
+      }
+    },
+  }
 };
 </script>
 
 <template>
   <div>
-    <ListHeader :title="title" @refresh="" :routePath="routePath" />
+    <ListHeader :title="title" @refresh="getRecommendedIcecream" :routePath="routePath" />
     <div class="column">
       <div class="card">
         <span v-if="errorMessage">{{errorMessage}}</span>
